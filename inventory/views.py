05a1eb5505febbf24a102c1960f1e8cf5ccd560a
Kavon Cacho <kavoncacho@gmail.com>
2021-09-15 22:31:58 -0700

    create 'apps' with pages for ICETRACK's different functions

--- inventory/views.py
+++ inventory/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("ICETRACK home")
+    return HttpResponse("ICETRACK inventory")
