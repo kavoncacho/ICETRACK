bad41c9fffc4f9bbb6d7d514e7c7e165577f8d72
Kavon Cacho <kavoncacho@gmail.com>
2021-10-04 17:48:20 -0700

    create database models for orderentry and inventory

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class itemFlavor (models.Model):
+    item_Flavor = models.CharField(max_length=30)
+
+class sizeCounts (models.Model):
+    half_Pint_Count = models.IntegerField(default=30)
+    one_Quart_Count = models.IntegerField(default=30)
+    pint_Count = models.IntegerField(default=30)
+    half_Gallon_Count = models.IntegerField(default=30)
+    gallon_Count = models.IntegerField(default=30)
+
+#Counting how many are dedicated to unfilled orders
+class toBeSent (models.Model):
+    to_Be_Sent = models.IntegerField(default=0)
+
+class receipts (models.Model):
+    receipts = models.CharField(max_Length = 150)
+
+#Keeps track of who makes changes to the database
+class lastChangeMade (models.Model):
+    last_Change_Made = models.CharField(max_length = 100)
+
--- orderentry/models.py
+++ orderentry/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from inventory import itemFlavor, sizeCounts
 
-# Create your models here.
+class customerName (models.Model):
+    customer_name = models.CharField(max_length=30)
+
+class shippingAddress (models.Model):
+    shipping_address = models.CharField(max_length=60)
+
+class billingAddress (models.Model):
+    billing_address = models.CharField(max_length=60)
+
+class customerStatus (models.Model):
+    customer_status = models.CharField(max_length = 20)
+
+class orderItemFlavor (models.Model):
+    order_Item_Flavor = models.ForeignKey(itemFlavor,on_delete=models.CASCADE)
+
+class orderItemSize (models.Model):
+    order_Item_Flavor = models.ForeignKey(sizeCounts,on_delete=models.CASCADE)
+
+class orderItemQuantity (models.Model):
+    order_Item_Quantity = models.IntegerField(default=0)
+
+class itemShipping (models.Model):
+    order_Item_Shipping = models.CharField(max_length = 20)
+
+class orderDate(models.Model):
+    order_date = models.DateTimeField('order date')
+
+class totalCost(models.Model):
+    total_Cost = models.IntegerField(default=0)
