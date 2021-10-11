b1f4b1cc0353ecdff3e667a4651102a128f4d0c6
Kavon Cacho <kavoncacho@gmail.com>
2021-10-10 22:08:55 -0700

    more optimization

--- orderentry/models.py
+++ orderentry/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 from inventory.models import sizeCounts
 
+class orderInfo (models.Model):
+    class Meta:
+        verbose_name = "Order Information"
+        verbose_name_plural = "Order Information"
+    
+    order_Item_Flavor = models.ForeignKey('inventory.itemFlavor', on_delete=models.CASCADE)
+    order_Size_Quantity = models.ForeignKey('inventory.sizeCounts', on_delete=models.CASCADE, default = 0)
+    
+    def __str__(self):
+        return '%s, %s, $%s, %s' % (self.order_Item_Flavor, self.order_Size_Quantity, self.total_Cost, self.order_date)
+
 class customerInfo (models.Model):
     class Meta:
         verbose_name = "Customer Information"
@@ -11,21 +22,23 @@ class customerInfo (models.Model):
     billing_address = models.CharField(max_length=60)
     customer_status_choices = [('PREFFERED','preferred'),('OKAY', 'okay'),('SHAKY', 'shaky')]
     customer_status = models.CharField(max_length=30, choices = customer_status_choices, default="PREFERRED")
+    order = models.ForeignKey(orderInfo, on_delete=models.CASCADE, default=2)
 
     def __str__(self):
         return self.customer_name
-    
-class orderInfo (models.Model):
+
+class invoice (models.Model):
     class Meta:
-        verbose_name = "Order Information"
-        verbose_name_plural = "Order Information"
-    
-    order_Item_Flavor = models.ForeignKey('inventory.itemFlavor', on_delete=models.CASCADE)
-    order_Size_Quantity = models.ForeignKey('inventory.sizeCounts', on_delete=models.CASCADE, default = 0)
+        verbose_name = "Invoice"
+        verbose_name_plural = "Invoices"
+
+    customer = models.ForeignKey(customerInfo, on_delete=models.CASCADE)
+    order = models.ForeignKey(orderInfo, on_delete=models.CASCADE)
     order_Item_Shipping_Choices = [('STANDARD','standard'),('EXPEDITED','expedited'),('2-DAY','2-day')]
     order_Item_Shipping = models.CharField(max_length = 20, choices = order_Item_Shipping_Choices, default="STANDARD")
     order_date = models.DateTimeField('order date')
     total_Cost = models.IntegerField(default=0)
 
     def __str__(self):
-        return '%s, %s, $%s, %s' % (self.order_Item_Flavor, self.order_Size_Quantity, self.total_Cost, self.order_date)
+        return '%s Invoice' % (self.customerInfo.customer_name)
+    
