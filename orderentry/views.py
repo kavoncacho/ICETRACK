7181b0324af57606ee9bddc616e50cfda5841c55
Jacob Le <jellyjaacob@Jacobs-MacBook-Air-2.local>
2021-10-23 22:21:46 -0700

    added to order entry html

--- orderentry/views.py
+++ orderentry/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import customerInfo, orderInfo, invoice
+from inventory.models import sizeCounts, item
 
 def index(request):
+
     return render(request, 'orderentry.html')
