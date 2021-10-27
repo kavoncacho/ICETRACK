9b2467e174a0bdf050b3e144e584e64130a5fd37
Kavon Cacho <kavoncacho@gmail.com>
2021-10-26 18:22:56 -0700

    more html tweaking

--- orderentry/forms.py
+++ orderentry/forms.py
@@ -3,12 +3,11 @@ from orderentry.models import customerInfo, orderInfo
 
 class customerForm(forms.ModelForm):
 
-    customer_first_name = forms.CharField(max_length=30)
-    customer_last_name = forms.CharField(max_length=30)
-    shipping_address = forms.CharField(max_length=60)
-    billing_address = forms.CharField(max_length=60)
+    customer_first_name = forms.CharField(max_length=30, label='First Name')
+    customer_last_name = forms.CharField(max_length=30, label='Last Name')
+    shipping_address = forms.CharField(max_length=60, label='Shipping Address')
+    billing_address = forms.CharField(max_length=60, label='Billing Address')
 
     class Meta:
         model = customerInfo
         fields = ('customer_first_name','customer_last_name','shipping_address', 'billing_address',)
-    
