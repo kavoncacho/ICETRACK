d72206e9191561d1444ad0e5d2229949fa482e8c
Jacob Le <jellyjaacob@Jacobs-MacBook-Air-2.local>
2021-11-14 19:50:40 -0800

    worked on shipment tracking as a whole

--- shipmenttracking/views.py
+++ shipmenttracking/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import shipmenttracking
+from shipmenttracking.forms import shipmentTrackForm 
 
-def index(request):
-    return HttpResponse("ICETRACK shipments")
+def getShipmentCode(request):
+    form = shipmentTrackForm(request.POST)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            shipmenttracking.forms.trackingCode = form.cleaned_data['trackingCode']
+    else:
+        form = shipmentTrackForm()
+    
+    return render(request, 'shipmenttracking.html', {'form' : form})
