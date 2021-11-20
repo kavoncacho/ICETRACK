04e7762d053021ed326f27c35a6374f5c64af112
Kavon Cacho <kavoncacho@gmail.com>
2021-11-20 12:31:10 -0800

    Remodel inventory database and start redoing order entry

--- orderentry/forms.py
+++ orderentry/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from orderentry.models import customerInfo, orderInfo
 
-class customerForm(forms.ModelForm):
+class customerInfo(forms.ModelForm):
 
     customer_first_name = forms.CharField(max_length=30, label='First Name')
     customer_last_name = forms.CharField(max_length=30, label='Last Name')
@@ -12,15 +12,14 @@ class customerForm(forms.ModelForm):
         model = customerInfo
         fields = ('customer_first_name','customer_last_name','shipping_address', 'billing_address',)
         
-class customerInformation(forms.ModelForm):
+class customerOrder(forms.ModelForm):
     order_Item_Flavor = forms.ChoiceField(choices=[('CHOCOLATE','chocolate'),('VANILLA', 'vanilla'),('COOKIESNCREME', 'cookiesncreme'), ('STRAWBERRY', 'strawberry')])
     half_Pint_Count = forms.IntegerField()
     one_Quart_Count = forms.IntegerField()
     pint_Count = forms.IntegerField()
     half_Gallon_Count = forms.IntegerField()
     gallon_Count = forms.IntegerField()
-    cost = forms.IntegerField()
 
     class Meta:
         model = orderInfo
-        fields = ('half_Pint_Count', 'one_Quart_Count', 'pint_Count', 'half_Gallon_Count', 'gallon_Count', 'cost',)
+        fields = ('half_Pint_Count', 'one_Quart_Count', 'pint_Count', 'half_Gallon_Count', 'gallon_Count',)
